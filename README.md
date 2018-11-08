0882bc2abfd72159f11c6528e5d1b1d6319b74c2
Nico Andrade <nicolas.andrade@ssense.com>
2018-11-08 15:15:22 -0500

    Reaadme

---
+++ README.md
@@ -0,0 +1,19 @@
+
+ **Challenge:**
+ THERE ARE _3_ TASKS TO DO IN THIS CODE
+ 
+ * Task ONE: Make it so every vowel in the string is replaced by a lowercase x
+ 
+ * Task TWO: Make it so when a string ends in a consonant the string is finished with ", right?"
+ 
+ * Task THREE: Make it so each string starts with the number of characters in the string in parenthesis, so for example:
+ 
+ ```(18) this is my example```
+
+ `18` because `this is my example` has 18 characters
+ 
+
+
+**GOAL:**
+ * Make it so the feature flags can be turned on and off, independently, and that the behaviour is still respected for each task.
+ * Ideally the complexity of the code should NOT increase exponentially.
--- index.php
+++ index.php
@@ -11,21 +11,24 @@
  *
  * Challenge:
  *
- * THERE ARE 2 TASKS TO DO IN THIS CODE
+ * THERE ARE 3 TASKS TO DO IN THIS CODE
  *
  * TASK ONE: Make it so every vowel in the string is replaced by a lowercase x
  *
  *
  * TASK TWO: Make it so when a string ends in a consonant the string is finished with ", right?"
  *
+ * TASK THREE: Make it so each string starts with the number of characters in the string in parenthesis, so for example:
+ * (18) this is my example
+ *
+ * 18 because "this is my example" has 18 characters
+ *
  *
  *
  * GOAL:
- * Make it so the feature flags can be turned on and off, independently, and that the behaviour is still respected.
+ * Make it so the feature flags can be turned on and off, independently, and that the behaviour is still respected for each task.
  *
- *
- *
- *
+ * Ideally the complexity of the code should NOT increase exponentially.
  *
  *
  * Quick help:
